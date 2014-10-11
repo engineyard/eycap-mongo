944136791794380928efbedabd02856f6e22fa8d
Tyler Bird <tyler.bird@gmail.com>
2014-10-11 08:53:28 -0600

    rename cookbook

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
-name              "mongo-tlq"
-maintainer        "Ben Dixon"
-maintainer_email  "ben@talkingquickly.co.uk"
+name              "eycap-mongo"
+maintainer        "Ben Dixon, Tyler Bird"
+maintainer_email  "ben@talkingquickly.co.uk, tbird@engineyard.com"
 description       "Installs the 10gen mongo server package"
 version           "0.0.4"
 
-recipe "mongo-tlq", "10gen mongo server package"
+recipe "eycap-mongo", "10gen mongo server package"
 
 supports "ubuntu"
